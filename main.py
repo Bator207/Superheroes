4a76f4761e94e3a0d1070c2a91e93a7f5fcbdb79
Albert Gil <albert.gil85@gmail.com>
2021-08-12 22:52:05 +0200

    Añado comprobacion de fecha actual/bin/python3

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 from config import *
+from datetime import datetime
 
-def comprobarNombre(nom,dic):
-    letra=nom[0].upper()
-    if letra in dic:
-        return dic[letra]
-    else:
-        print('{} no es una letra'.format(nom[0]))
+def frase(sh,co,ar,sp):
+    return ('Soy {} {}, mi poder es el {} y voy a luchar contra la injusticia con mi {}!!'.format(sh,co,sp,ar))
 
-def comprobarNumero(strNumero,dic):
-    try:
-        numero=int(strNumero)
-        if numero in dic.keys():
-            return dic[numero]
-    except:
-        print('{} no es un numero'.format(strNumero))
+def comprobarNombre(dic,frase):
+    salida=False
+    cadena=input(frase)
+    while not salida:
+        if cadena == '':
+            print('ERROR!!!! No puede ser vacio')    
+            cadena=input(frase)
+        letra=cadena[0].upper()
+        if letra in dic:
+            salida=True
+        else:
+            print('{} no es una letra'.format(cadena[0]))
+            cadena=input(frase)
+    return dic[letra]
+
+def comprobarNumero(dic,frase,tipo):
+    salida=False
+    cadena=input(frase)
+    while not salida:
+        if cadena == '':
+            print('ERROR!!!! No puede ser vacio')    
+            cadena=input(frase) 
+        if tipo=='D':
+            if len(cadena)<=2 and cadena!='0':
+                try:
+                    numero=int(cadena)
+                    salida=True
+                except:
+                    print('{} no es un número'.format(cadena))
+                    cadena=input(frase)
+            else:
+                print('ERROR!!!! No puede tener mas de 2 números y no ser 0')    
+                cadena=input(frase)
+        if tipo=='A':
+            if len(cadena)==4 and cadena!='0':
+                try:
+                    numero=int(cadena)
+                    salida=True
+                except:
+                    print('{} no es un número'.format(cadena))
+                    cadena=input(frase)
+            else:
+                print('ERROR!!!! El año tiene que tener 4 números o no ser 0')    
+                cadena=input(frase)
+    return numero
 
 def ultimoNumero(strNumero):
     try:
@@ -21,21 +56,45 @@ def ultimoNumero(strNumero):
     except:
         print('{} no es un numero'.format(strNumero))
 
-def frase(sh,co,ar,sp):
-    return ('Soy {} {}, mi poder es el {} y voy a luchar contra la injusticia con mi {}!!'.format(sh,co,sp,ar))
+def comprobarFecha(d,m,a):
+    actual=datetime.now()
+    if a==actual.year:
+        if m<=actual.month:
+            if d<=actual.day:
+                #fecha correcta
+                print('fecha introducida {} {} {}'.format(d,m,a))
+                print('fecha actual {} {} {}'.format(actual.day,actual.month,actual.year))
+            else:
+                print('Todavia no has nacido')
+                # comprobar de nuevo dia
+                print('fecha introducida {} {} {}'.format(d,m,a))
+                print('fecha actual {} {} {}'.format(actual.day,actual.month,actual.year))
+        else:
+            print('llegaste a else mes')
+            #comprobar de nuevo mes
+            print('fecha introducida {} {} {}'.format(d,m,a))
+            print('fecha actual {} {} {}'.format(actual.day,actual.month,actual.year))
+    elif a>1900 and a<actual.year:
+        # fecha correcta, comprobar nombres
+        print('llegaste a elif año')
+        print('fecha introducida {} {} {}'.format(d,m,a))
+        print('fecha actual {} {} {}'.format(actual.day,actual.month,actual.year))
+    else:
+        # comprobar año
+        print('llegaste a else año')
+        print('fecha introducida {} {} {}'.format(d,m,a))
+        print('fecha actual {} {} {}'.format(actual.day,actual.month,actual.year))
+
 
 if __name__=='__main__':
-    superheroe=''
-    nom=input('Introduce tu nombre: ')
-    cognom=input('Introduce tu apellido: ')
-    diaN=input('Introduce el dia que naciste: (Formato númerico)')
-    mesN=input('Introduce el mes que naciste: (Formato númerico)')
-    anyN=input('Introduce el año que naciste: (Formato númerico)')
-
-    superheroe+=comprobarNombre(nom,nombre)
-    superheroe+=' '
-    superheroe+=comprobarNombre(cognom,apellido)
-    arma=comprobarNumero(diaN,dia)
-    superpoder=comprobarNumero(mesN,mes)
-    traje=comprobarNumero(ultimoNumero(anyN),any)
-    print(frase(superheroe,traje,arma,superpoder))
+    fecha=[]
+    comprobarFecha(13,8,2021)
+    # superheroe="{} {}".format(comprobarNombre(nombre,'Introduce tu nombre: '),comprobarNombre(apellido,'Introduce tu apellido: '))
+    # f=comprobarNumero(dia,'Introduce el dia que naciste: (Formato númerico)','D')
+    # fecha.append(f)
+    # f=comprobarNumero(mes,'Introduce el mes que naciste: (Formato númerico)','D')
+    # fecha.append(f)
+    # f=comprobarNumero(any,'Introduce el año que naciste: (Formato númerico)','A')
+    # fecha.append(f)
+    # print(fecha)
+    # print(frase(superheroe,traje,arma,superpoder))
