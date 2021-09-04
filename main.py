a3a1d1068cfb72f1dd410f2cf45cbfbe4102fd7d
Albert Gil <albert.gil85@gmail.com>
2021-09-04 21:17:32 +0200

    Agregamos comprobación de fecha

--- main.py
+++ main.py
@@ -26,99 +26,69 @@ def comprobarNombre(dic,frase):
     return dic[letra]
 
 # Funcion donde se pide la fecha de nacimiento
-def fecha():
+def pedirFecha():
     fecha=[]
     tipo=('año','mes','dia')
-    nacimiento=False
+    actual=datetime.now()
+
     for i in tipo:
-        frase='Introduce el {} que naciste: (Formato númerico) '.format(tipo)
+        nacimiento=False
+        frase='Introduce el {} que naciste: (Formato númerico) '.format(i)
         while not nacimiento:
             cadena=Input(frase,line=1,column=5)
-            if cadena=='':
-                Print('ERROR!!!! No puede ser vacio',line=25,column=1,style='bold',color='yellow', back='red')
-            if i == 'dia':
-                pass
+            clearLine(25)
+            try:
+                numero=int(cadena)
+            except:
+                Print('{} no es un número'.format(cadena),line=25,column=1,style='bold',color='yellow', back='red')
+                continue
+            if i == 'año':
+                if len(cadena)==4 and cadena!='0':
+                    if numero>=1920 and numero<=actual.year:
+                        fecha.append(numero)
+                        nacimiento=True
+                    else:
+                        Print("ERROR!!!! Año incorrecto",line=25,column=1,style='bold',color='yellow', back='red')
+                        continue
+                else:
+                    Print('ERROR!!!! No puede ser 0 o tener una longuitud diferente de 4 digitos',line=25,column=1,style='bold',color='yellow', back='red')
+                    continue
+            elif i == 'mes':
+                if len(cadena)<=2 and cadena!='0':
+                    if fecha[0]==actual.year:
+                        if numero<=actual.month:
+                            fecha.append(numero)
+                            nacimiento=True
+                        else:
+                            Print('ERROR!!!! Mes incorrecto',line=25,column=1,style='bold',color='yellow', back='red')
+                            continue
+                    else:
+                        fecha.append(numero)
+                        nacimiento=True
+                else:
+                    Print('ERROR!!!! No puede ser 0 o tener una longuitud mayor de 2 a digitos',line=25,column=1,style='bold',color='yellow', back='red')
+                    continue
+            else:
+                #Falta comprobar que esta en el rango
+                if len(cadena)<=2 and cadena!='0':
+                    if fecha[1]==actual.month:
+                        if numero<=actual.day:
+                            fecha.append(numero)
+                            nacimiento=True
+                        else:
+                            Print('ERROR!!!! Dia incorrecto',line=25,column=1,style='bold',color='yellow', back='red')
+                            continue
+                    else:
+                        fecha.append(numero)
+                        nacimiento=True
+                else:
+                    Print('ERROR!!!! No puede ser 0 o tener una longuitud mayor de 2 a digitos',line=25,column=1,style='bold',color='yellow', back='red')
+                    continue
+    return fecha
 
-# def pedirFecha():
-#     fecha=[]
-#     tipo=('año','mes','dia')
-#     for i in range (3):
-#         frase='Introduce el {} que naciste: (Formato númerico) '.format(tipo[i])
-#         strNumero=Input(frase,line=1,column=5)
-#         cadena=__comprobarNumero(frase,tipo[i],strNumero)
-#         fecha.append(cadena)
-#     return fecha
-
-# # Función para comprobar la fecha
-# def __comprobarNumero(frase,tipo,cadena):
-#     salida=False
-#     while not salida:
-#         if cadena == '':
-#             Print('ERROR!!!! No puede ser vacio',line=25,column=1,style='bold',color='yellow', back='red')
-#             cadena=Input(frase,line=1,column=5)
-#         clearLine(25)
-#         if tipo=='dia' or tipo=='mes':
-#             if len(cadena)<=2 and cadena!='0':
-#                 try:
-#                     numero=int(cadena)
-#                     salida=True
-#                 except:
-#                     Print('{} no es un número'.format(cadena),line=25,column=1,style='bold',color='yellow', back='red')
-#                     cadena=Input(frase,line=1,column=5)
-#                 clearLine(25)
-#             else:
-#                 Print('ERROR!!!! El {} no puede tener mas de 2 números y no ser 0'.format(tipo),line=25,column=1,style='bold',color='yellow', back='red')
-#                 cadena=Input(frase,line=1,column=5)
-#             clearLine(25)
-#         if tipo=='año':
-#             if len(cadena)==4 and cadena!='0':
-#                 try:
-#                     numero=int(cadena)
-#                     salida=True
-#                 except:
-#                     Print('{} no es un número'.format(cadena),line=25,column=1,style='bold',color='yellow', back='red')
-#                     cadena=Input(frase,line=1,column=5)
-#                 clearLine(25)
-#             else:
-#                 Print('ERROR!!!! El año tiene que tener 4 números o no ser 0',line=25,column=1,style='bold',color='yellow', back='red')
-#                 cadena=Input(frase,line=1,column=5)
-#             clearLine(25)
-#     return numero
-
-# def ultimoNumero(strNumero):
-#     strNumero=str(strNumero)
-#     return int(strNumero[-1])
-
-# def comprobarFecha(fecha):
-#     actual=datetime.now()
-#     nacimiento=False
-#     while not nacimiento:
-#         if fecha[0]>1900 and fecha[2]<=(actual.year-1):
-#             nacimiento=True
-#         elif fecha[2]<=actual.year:
-#             if fecha[1]<=actual.month:
-#                 if fecha[2]<=actual.day:
-#                     # Fecha correcta
-#                     nacimiento=True
-#                 else:
-#                     # Fecha incorrecta, volver a comprobar de nuevo dia
-#                     Print('Todavia no has nacido',line=25,column=1,style='bold',color='yellow', back='red')
-#                     strNumero=Input('Introduce el {} que naciste: (Formato númerico) '.format('dia'),line=1,column=5)
-#                     fecha[2]=__comprobarNumero('Introduce el {} que naciste: (Formato númerico)'.format('dia'),'dia',strNumero)
-#                 clearLine(25)
-#             else:
-#                 # Fecha incorrecta, volver a comprobar de nuevo mes
-#                 Print('Todavia no has nacido',line=25,column=1,style='bold',color='yellow', back='red')
-#                 strNumero=Input('Introduce el {} que naciste: (Formato númerico) '.format('mes'),line=1,column=5)
-#                 fecha[1]=__comprobarNumero('Introduce el {} que naciste: (Formato númerico)'.format('mes'),'mes',strNumero)
-#             clearLine(25)
-#         else:
-#             # Fecha incorrecta, volver a comprobar de nuevo mes
-#             Print('Todavia no has nacido',line=25,column=1,style='bold',color='yellow', back='red')
-#             strNumero=Input('Introduce el {} que naciste: (Formato númerico) '.format('año'),line=1,column=5)
-#             fecha[0]=__comprobarNumero('Introduce el {} que naciste: (Formato númerico)'.format('año'),'año',strNumero)
-#         clearLine(25)
-#     return fecha
+def ultimoNumero(strNumero):
+    strNumero=str(strNumero)
+    return int(strNumero[-1])
 
 def armaTrajeSuperpoder(fecha):
     sh=[]
@@ -132,6 +102,5 @@ if __name__=='__main__':
     limpiar()
     superheroe="{} {}".format(comprobarNombre(nombre,'Introduce tu nombre: '),comprobarNombre(apellido,'Introduce tu apellido: '))
     f=pedirFecha()
-    f=comprobarFecha(f)
     sh=armaTrajeSuperpoder(f)
     Print(frase(superheroe,sh[2],sh[0],sh[1]),line=5,column=5)
